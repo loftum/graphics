8f39dd06ed692151f566176b142bb0d1226b24ca
loftum <loftum@gmail.com>
2019-08-30 08:23:45 +0200

    Teapot

---
+++ Teapot/Teapot/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Teapot
+//
+//  Created by Hans Olav Loftum on 30/08/2019.
+//  Copyright © 2019 Hans Olav Loftum. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
